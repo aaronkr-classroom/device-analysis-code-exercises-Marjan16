a7a116168e0e213eac1ca0d07d6c381c1f6792f8
Marjan16 <marjanrahmanarthi@gmail.com>
2026-04-02 15:12:15 +0900

    Add files via upload

---
+++ hehe.py
@@ -0,0 +1,18 @@
+#4_loops.py
+#math
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 % 3)
+print(10 // 3) #floor division
+print(10 ** 3) #exp
+print(10 == 3)
+print(10 != 3)
+print(10 >= 3)
+print(10 <= 3)
+print(10<3 or 3<10)
+print(10<3 and 3<10)
+print(not 10<3)
+
+
